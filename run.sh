40ca2fc71ee3c4836ff46671319c4f50594aa291
Philip Sahli <philip@sahli.net>
2014-10-29 05:39:26 +0100

    init to update

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-docker run -i -d -p 10056:5432 -e DB_NAME=odoo -e DB_USER=odoo -e PASSWORD=odoo --name odoo-db postgresql-test || echo "already running"
-# docker run -i -p 80:80 --link odoo-db:db -t --name odoo docker-odoo /bin/bash || echo "already running"
-docker run -i -d -p 10080:80 --link odoo-db:db -t --name odoo docker-odoo || echo "already running"
+(docker run -i -d -p 10056:5432 -e DB_NAME=odoo -e DB_USER=odoo -e PASSWORD=odoo --name odoo-db postgresql-test || echo "already running") && docker start odoo-db
+#(docker run -i -p 10080:80 --link odoo-db:db -t --name odoo docker-odoo /bin/bash || echo "already running") && docker start odoo
+(docker run -i -d -p 10222:22 -p 10080:80 --link odoo-db:db -t -e SSH_PUBKEY=$SSH_PUBKEY --name odoo docker-odoo || echo "already running") && docker start odoo
