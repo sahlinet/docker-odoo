f75a1599dbb86a4ad612a38d8773e9f351736e14
Philip Sahli <philip@sahli.net>
2018-02-09 22:11:28 +0100

    fix new download url

--- startup_app.sh
+++ startup_app.sh
@@ -2,4 +2,4 @@ j2 /etc/odoo/openerp-server.conf | sponge /etc/odoo/openerp-server.conf
 
 /usr/sbin/nginx -g "daemon off;" &
 
-sudo -i -u odoo /opt/odoo-8.0.0/openerp-server --config /etc/odoo/openerp-server.conf -u base,web --without-demo=all
+sudo -i -u odoo /opt/odoo-8.0-20171009/openerp-server --config /etc/odoo/openerp-server.conf -u base,web --without-demo=all
